48546ddff6630bd9ec1df8fbdc3365ec10ad63c6
utpolra1 <utpolra@gmail.com>
2024-04-30 15:41:24 +0600

    update to update card mathod

--- index.js
+++ index.js
@@ -55,7 +55,6 @@ async function run() {
       res.send.result;
     })
 
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
