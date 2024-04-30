e40c66620c801a5e2f157fd5b975a83bd7a5f10f
utpolra1 <utpolra@gmail.com>
2024-04-30 15:40:02 +0600

    update post method

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3i9ecp5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
