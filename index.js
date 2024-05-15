9281de31040dc3358edd3405c31e2bc908ad1dd4
utpolra1 <utpolra@gmail.com>
2024-05-15 18:40:12 +0600

    update to comment method update

--- index.js
+++ index.js
@@ -69,7 +69,14 @@ async function run() {
       res.send(result);
     });
 
-   
+    //Update Comment data
+    app.get("/comments/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const cursor = await commentCollection.findOne(query);
+      res.send(cursor);
+    });
 
     
     
