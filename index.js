4d955ba1f1d7fd80f8c78d9726faa278d15f36e3
utpolra1 <utpolra@gmail.com>
2024-05-15 18:35:53 +0600

    update blog get and post method

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
     const blogCollection = client.db("newBlog").collection("blog");
     const wishlistCollection = client.db("newBlog").collection("wishlist");
     const commentCollection = client.db("newBlog").collection("comments");
+    const featuredblogsCollection=client.db("newBlog").collection("featuredblogs");
 
     app.get("/blog", async (req, res) => {
       const cursor = blogCollection.find();
@@ -34,61 +35,12 @@ async function run() {
       res.send(result);
     });
 
-    //update get data
-    app.get("/blog/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const cursor = await blogCollection.findOne(query);
-      res.send(cursor);
-    });
+   
 
-    app.get("/wishlist", async (req, res) => {
-      const cursor = wishlistCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    });
+    
 
-    //add New Commet
-    app.get("/comments", async (req, res) => {
-      const cursor = commentCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-
-    app.post("/comments", async (req, res) => {
-      const newProduct = req.body;
-      // console.log(newProduct);
-      const result = await commentCollection.insertOne(newProduct);
-      res.send(result);
-    });
-
-    //Update Comment data
-    app.get("/comments/:id", async (req, res) => {
-      const id = req.params.id;
-      console.log(id);
-      const query = { _id: new ObjectId(id) };
-      const cursor = await commentCollection.findOne(query);
-      res.send(cursor);
-    });
-
-    app.post("/wishlist", async (req, res) => {
-      const { title, shortdescription, email ,image} = req.body;
-      const newBlog = { title, shortdescription, email, image };
-      // console.log(newBlog);
-      const result = await wishlistCollection.insertOne(newBlog);
-      // console.log(result)
-      res.send(result);
-    });
-
-    //
-    app.get("/wishlist/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      // console.log(id);
-      const cursor = await wishlistCollection.findOne(query);
-      res.send(cursor);
-    });
-
+    
+    
     app.post("/blog", async (req, res) => {
       const newProduct = req.body;
       // console.log(newProduct);
@@ -96,32 +48,7 @@ async function run() {
       res.send(result);
     });
 
-    // delete mathod
-    app.delete("/wishlist/:id", async (req, res) => {
-      const id = req.params.id;
-      // console.log("marking", id);
-      const query = { _id: new ObjectId(id) };
-      const result = await wishlistCollection.deleteOne(query);
-      res.send(result);
-    });
-
-    //update mthod
-    app.put("/blog/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const options = { upsert: true };
-      const updateDoc = req.body;
-      const update = {
-        $set: {
-          title: updateDoc.title,
-          image: updateDoc.image,
-          shortdescription: updateDoc.shortdescription,
-          longDescription: updateDoc.longDescription,
-        },
-      };
-      const result = await blogCollection.updateOne(query, update, options);
-      res.send(result);
-    });
+   
 
     //Comment Update
     app.put("/comments/:id", async (req, res) => {
